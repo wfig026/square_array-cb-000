bd41acb6486b9bd69f9bd5d4d4e9646d12abce6a
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 00:25:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   numbersSquard = []
   array.each do |number|
-    result.push(number * number)
+    numbersSquard.push(number * number)
   end
   numbersSquard
 end
